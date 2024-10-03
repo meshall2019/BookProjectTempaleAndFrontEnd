6b214a2fb2dc095a266a986b93d6cf47aa58f2d6
DELL <DELL@DESKTOP-BL29OQH>
2024-10-03 04:11:44 +0300

    Add project files.

---
+++ BooksProject/Areas/Admin/Controllers/AdminPageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BooksProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminPageController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BooksProject/Components/CategoryMenue.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BookStoreProject.Components
+{
+    public class CategoryMenue:ViewComponent
+    {
+        
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            return View();
+           
+        }
+
+
+
+
+
+
+    }
+}
